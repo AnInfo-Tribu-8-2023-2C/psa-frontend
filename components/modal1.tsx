c9604a158bbf4d6354e31dfbd3b382c3024375ac
Adbonino <abonino88@gmail.com>
2023-11-27 20:05:07 -0300

    Create modal1.tsx

---
+++ components/modal1.tsx
@@ -0,0 +1,12 @@
+const Modal1 = ({ isOpen, onClose, children }) => {
+    return(
+         <div className="modal-container" style={{ display: isOpen ? 'grid' : 'none'}}>
+            <div className='modal-body'>
+               <button className='modal-close' onClick={onClose}>X</button> 
+               {children}
+               </div>
+         </div>
+    );
+}
+
+export default Modal1;
