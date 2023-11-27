1f66ac17427f0c7318e8c2ed3613239f28388974
Adbonino <abonino88@gmail.com>
2023-11-27 20:06:11 -0300

    Create modalSiNo.tsx

---
+++ components/modalSiNo.tsx
@@ -0,0 +1,14 @@
+
+const ModalSiNo = ({ isOpen, onClose, children }) => {
+  
+   return(
+       <div className="modal-container" style={{ display: isOpen ? 'grid' : 'none'}}>
+          <div className='modal-body'>
+             <button className='modal-close' onClick={onClose}>X</button> 
+             {children}
+          </div>
+       </div>
+  );
+}
+
+export default ModalSiNo;
