0b76b5cf072fb3eb3a500c7441910e379819ee27
abonino@gmail.com <abonino88@gmail.com>
2023-11-29 09:01:25 -0300

    add proyectos

--- components/layout.tsx
+++ components/layout.tsx
@@ -17,6 +17,10 @@ export default function Layout({ children }: { children: any }) {
     {
       href: "/productos",
       title: "Productos",
+    },
+    {
+      href: "/proyectos",
+      title: "Proyectos",
     }
   ]
 
