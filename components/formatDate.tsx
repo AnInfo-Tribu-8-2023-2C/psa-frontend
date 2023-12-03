d8942858b721448b84ea8f0b3fdd3137c93d013d
Adbonino <abonino88@gmail.com>
2023-12-02 23:15:54 -0300

    Update formatDate.tsx

--- components/formatDate.tsx
+++ components/formatDate.tsx
@@ -3,7 +3,7 @@ import { parseISO, format } from "date-fns";
 export default function FormatDate({ dateString }:{dateString: any}){
     const date = parseISO(dateString);
 
-    if (date === null) {return (<time>   </time>)} else {
+    if (dateString === null) {return (<time>   </time>)} else {
     return(
         <time>{format(date, 'LLL d, yyyy' )}</time>
     )}
