a0cb43e66cafb772222f07d66441c294d0609f26
gonzashima <gonza.shima@gmail.com>
2023-12-04 02:38:46 -0300

    ahora se puede modificar proyectos

--- components/isLeaderNull.tsx
+++ components/isLeaderNull.tsx
@@ -3,6 +3,6 @@ import { parseISO, format } from "date-fns";
 export default function IsLeaderNull({ lider }:{lider: any}){
     if (lider === null) {return (<>   </>)} else {
     return(
-        <>{lider['id']}</>
+        <>{lider['nombre'] + " " + lider['apellido']}</>
     )}
 }
