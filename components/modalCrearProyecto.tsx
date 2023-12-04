5071af057e3573e31ce6bbba5bbbfd4838f1be92
abonino@gmail.com <abonino88@gmail.com>
2023-12-04 09:49:43 -0300

    update modalCrearProyecto

--- components/modalCrearProyecto.tsx
+++ components/modalCrearProyecto.tsx
@@ -7,7 +7,7 @@ const ModalCrearProyecto = ({isOpen, onClose, guardarDatos,children}:{isOpen: bo
 
     const [nombre, setNombre] = useState("");
     const [descripcion, setDescripcion] = useState("");
-    const [lider, setLider] = useState("");
+    const [lider, setLider] = useState('1');
     const [estado, setEstado] = useState("NO_INICIADO");
     const [fechaIni, setFechaIni] = useState('');
     const [fechaFin, setFechaFin] = useState('');
@@ -62,7 +62,7 @@ const ModalCrearProyecto = ({isOpen, onClose, guardarDatos,children}:{isOpen: bo
                 <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Lider del Proyecto:</label>
                 <select className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="inputGroupSelect01"
                  onChange={(event)=>{setLider(event.target.value)}}>
-                    <option>...</option>
+                    <option value={lider}>...</option>
                     {
                         recursos.map( (recurso) => (
                             <option key={recurso['id']} value={recurso['id']}>{recurso['nombre']} {recurso['apellido']}</option>
