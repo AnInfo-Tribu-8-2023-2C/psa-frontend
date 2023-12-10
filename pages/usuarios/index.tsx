a0dc8c3fdd585e34f26bc5d5bcb0b84e9184a668
Sebastian Arroyo <larroyo@fi.uba.ar>
2023-12-10 19:38:28 -0300

    Fixed api request for usuarios / recursos

--- pages/usuarios/index.tsx
+++ pages/usuarios/index.tsx
@@ -9,7 +9,7 @@ export default function Usuarios() {
   const [list, setList] = useState([])
 
   useEffect(() => {
-    fetch("https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/754f50e8-20d8-4223-bbdc-56d50131d0ae/recursos-psa/1.0.0/m/api/recursos")
+    fetch("https://psa-backend-projectos.onrender.com/recursos")
       .then((res) => {
         return res.json()
       })
